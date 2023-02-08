b2be42533626cda638c584306a58f3af1b119caa
Ta David Yu <ta.yu.gamedev@gmail.com>
2023-02-08 04:12:41 +0100

    Added DYEngine as submodule, made a couple of cmake changes to enable DYEngine as library

---
+++ src/DYETechDemoApp.cpp
@@ -0,0 +1,25 @@
+#include "Core/Application.h"
+#include "AppEntryPoint.h"
+
+namespace DYE
+{
+	class DYETechDemoApp final : public Application
+	{
+	public:
+		DYETechDemoApp() = delete;
+		DYETechDemoApp(const DYETechDemoApp &) = delete;
+
+		explicit DYETechDemoApp(const std::string &windowName, int fixedFramePerSecond = 60)
+			: Application(windowName, fixedFramePerSecond)
+		{
+		}
+
+		~DYETechDemoApp() final = default;
+	};
+
+	Application *CreateApplication()
+	{
+		DYE_LOG("TEST");
+		return new DYETechDemoApp("Temp");
+	}
+}
